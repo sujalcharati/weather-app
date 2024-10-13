5fca8b839c0df5451bf76be723c30ddd7cbe91fa
sujalcharati <sujalc024@gmail.com>
2024-10-13 21:57:16 +0530

    search bar

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,37 @@ import './App.css'
 function App() {
 
   return (
-    <>
+   
       <div>
-        
+        <Searchbar/>
+      {/* <Weatherdisplay/> */}
       </div>
-    </>
+   
   )
 }
+function Searchbar(){
+  const [input,setInput] =useState('');
+    return (
+      <div className="flex justify-center items-center h-screen">
+      <input 
+        className="text-center border-black	border-solid"
+        type="text"
+        placeholder="Enter the location, country"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      </div>
+    )
+    
+}
+
+
+// function  Weatherdisplay(){
+//   const [address,setAddress] =useState('');
+//   return <div>
+//         {address}
+//         <h3>{country}</h3>
+//   </div>
+// }
 
 export default App
