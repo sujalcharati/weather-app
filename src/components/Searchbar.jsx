54c2af1a94620cecdc5ea3d983294d4f1e66ca97
sujalcharati <sujalc024@gmail.com>
2024-10-16 16:47:27 +0530

    functionality

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -2,36 +2,47 @@ import axios from "axios";
 import React from "react";
 import{ useState } from 'react'
 
-const api_key = "23dc27e2cd36408ca75130328240708";
-function Searchbar({ city, setCity }) {
-  const [city, setCity] = useState("");
+function Searchbar() {
+    const [city, setCity] = useState("");
+    const [weatherData, setWeatherData] = useState(null);
 
-  async function calculatetemp() {
-    const res = await axios.get(
-      "http://api.weatherapi.com/v1/current.json?key={api_key}&q=&aqi=yes"
-    );
-    return res.json();
-  }
-  return (
+    async function calculatetemp() {
+      const api_key = "23dc27e2cd36408ca75130328240708";
+      try {
+        const res = await axios.get(
+          `http://api.weatherapi.com/v1/current.json?key=${api_key}&q=${city}&aqi=yes`
+        );
+        setWeatherData(res);
+        console.log(res.data);
+      } catch (error) {
+        console.error("Error fetching the weather data", error);
+      }
+    }
+return (
     <div className="flex flex-col justify-center items-center h-screen">
-      <div>
-        <input
-          className="text-center border-black border-solid mb-4"
-          type="text"
-          placeholder="Enter the city"
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-        />
-        <button
-          className="bg-[#1da1f2] text-white rounded-sm"
-          onClick={calculatetemp}
-          type="submit"
-        >
-          get weather
-        </button>
-      </div>
-      <div className="text-center">{city}</div>
+        <div>
+            <input
+                className="text-center border border-black border-solid mb-4"
+                type="text"
+                placeholder="Enter the city"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+            />
+            <button
+                className="bg-[#1da1f2] text-white rounded-sm"
+                onClick={calculatetemp}
+                type="submit"
+            >
+                get weather
+            </button>
+        </div>
+        <div className="text-center">{city}</div>
+        {weatherData && weatherData.data && weatherData.data.current && (
+            <div>
+                    <h3>Temp:{weatherData.data.current.temp_c}°C</h3>
+            </div>
+        )}
     </div>
-  );
+);
 }
 export default Searchbar;
