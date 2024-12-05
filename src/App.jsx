22437b8c3b77672c8dd335bbb5ac698ccb4fe9e0
sujalcharati <sujalc024@gmail.com>
2024-12-05 09:24:49 +0530

    removed appbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ import Searchbar from "./components/Searchbar";
 function App() {
   return (
     <div className="background">
-      <div className="bg-slate-900 p-7 border">
-        hone
-      </div>
+      
       <Searchbar />
     </div>
   );
