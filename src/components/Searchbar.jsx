f51ecfcbef052860a8322d5d2910fa95b26f8bb5
sujalcharati <sujalc024@gmail.com>
2025-01-22 23:18:03 +0530

    plug in issue resolved

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,8 +1,6 @@
 import axios from "axios";
 import React from "react";
 import{ useState } from 'react'
-import dotenv from 'dotenv';
-dotenv.config();
 
 function Searchbar() {
     const [city, setCity] = useState('');
